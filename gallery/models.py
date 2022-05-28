c379fbefdd4e8d11762747ce7e1fd0d583619070
oyesa <mercyoyesa2018@gmail.com>
2022-05-28 13:19:27 +0300

    create methods for location class

--- gallery/models.py
+++ gallery/models.py
@@ -9,6 +9,20 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length = 80)
 
+    def save_location(self):
+      self.save()
+
+    def delete_location(self):
+      self.delete()
+
+    @classmethod
+    def update_location(cls, id, name):
+      cls.objects.filter(id=id).update(name=name)
+
+    @classmethod
+    def delete_location(cls):
+      cls.objects.all()
+
     def __str__(self):
         return self.name
 
