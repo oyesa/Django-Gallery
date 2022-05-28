312ee4fa2208f1a33034dd7b0db050dc3214a430
oyesa <mercyoyesa2018@gmail.com>
2022-05-28 13:23:04 +0300

    create methods for class category

--- gallery/models.py
+++ gallery/models.py
@@ -30,6 +30,16 @@ class Location(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length = 80)
 
+    def save_category(self):
+      self.save()
+
+    def delete_category(self):
+      self.delete()
+
+    @classmethod
+    def update_category(cls, id, name):
+      cls.objects.filter(id=id).update(name=name)
+
     def __str__(self):
         return self.name
 
