b844de72779aa3a24ff5b7b19b543b3b54cc17ae
oyesa <mercyoyesa2018@gmail.com>
2022-05-29 09:11:19 +0300

    update search function in gallery views

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
+import numpy as np
 from .models import Location, Category, Image
 from django.core.exceptions import ViewDoesNotExist
 
@@ -10,22 +12,47 @@ from django.core.exceptions import ViewDoesNotExist
 # Create your views here.
 def home(request):
   #get all images
-  images = Image.objects.all().order_by('-id')
-  locations = Location.objects.all()
-  categories = Category.objects.all()
-  return render(request, 'home.html', {"images":images,"locations": locations,"categories": categories})
+    images = Image.objects.all()
+    print(images[0].category)
+    arr= np.array(images) 
+    newarr = np.array_split(arr,3)
+    first = newarr[0]
+    second = newarr[1]
+    third = newarr[2]
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'home.html', {"first": first,"second": second,"third": third,"locations": locations,"categories": categories})
+
 
 
 #search_results function
-def search_results(request):
-    if 'category' in request.GET and request.GET["category"]:
-        search_term = request.GET.get("category").lower()
-        searched_images = Image.filter_by_category(search_term)
-        message = f"{search_term}"
-        locations = Location.objects.all()
-        categories = Category.objects.all()
-        return render(request, 'search.html',{"message":message, "categories": categories}) 
+def search(request):
+  if 'category' in request.GET and request.GET["category"]:
+    search = request.GET.get("category")
+    try:
+      category = Category.objects.get(name = search)
+      images = Image.search_image(category)
+      arr = np.array(images)
+      newarr = np.array_split(arr,3)
+      first = newarr[0]
+      second = newarr[1]
+      third = newarr[2]
+      message = f"Found {len(images)} image(s) in this category - {search}"
+      return render(request, 'search.html',{"message":message,"images": images,"first": first,"second": second,"third": third})
+    except ViewDoesNotExist:
+            message = "No items in this category " + search.upper()
+            categories = Category.objects.all()
+            return render(request, 'search.html',{"message":message, "categories": categories}) 
 
-    else:
-        message = "You haven't searched for any category"
-        return render(request, 'search.html',{"message":message})
+
+
+#display image in names location
+def location(request, location_id):
+    locations = Location.objects.all()
+    images = Image.objects.filter(location_id=location_id)
+    location = Location.objects.get(id=location_id)
+    title = location
+    return render(request, 'location.html', {'images': images, 'locations': locations, 'title': title})
+    
+
+
