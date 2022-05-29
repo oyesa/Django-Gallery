e06689ee4ec19e75a633c29bbba9d626a6dd3f79
oyesa <mercyoyesa2018@gmail.com>
2022-05-29 11:41:48 +0300

    update paths in gallery urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,5 @@
 from . import views
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +8,11 @@ from django.conf.urls.static import static
 urlpatterns=[
   path('', views.home, name='home'),
   path('search/', views.search, name='search'),
-  path('location/', views.location, name='location'),
-  path('category/', views.category, name='category')
+  re_path(r'^location/(?P<location_id>\d+)',views.location,name ='location'),
+  re_path(r'^category/(?P<category_id>\d+)',views.category,name ='category'),
+  re_path(r'^image/(?P<image_id>\d+)',views.image,name ='image'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
